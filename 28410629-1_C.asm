c206176893c6789032ee3ab318241a873f9be9aa
root <root@localhost.localdomain>
2019-02-09 23:12:27 +0000

    Own Multiple Code

--- 28410629-1_C.asm
+++ 28410629-1_C.asm
@@ -1,16 +1,26 @@
-global main
 extern printf
 
 section .data
-	msg db "Testing %i...", 0x0a, 0x00
+	msg	db	"Result : %i", 0x0a, 0x00
 
+section .text
+
+global main
 main:
-	push ebp
-	mov ebp, esp
-	push 123
-	push msg
-	call printf
-	mov eax, 0
-	mov esp, ebp
-	pop ebp
-	ret
+	mov	eax, 5		; uses general registery eax to contain value 5 
+	mov	ebx, 4		; uses general registery ebx to contain value 4
+	mov	ecx, 0		; uses general registery ecx to store result, gives initial value
+mult:	
+	add	ecx, eax	; add value of eax to ecx
+	dec	ebx		; decrements ebx to avoid infinite loop
+	cmp	ebx, 0		; compare ebx to 0
+	jg	mult		; jumps back to label mult if greater than 0
+	push 	ebp		; prologue
+	mov 	ebp, esp	; prologue
+	push 	ecx		; result is pushed first on to stack, due to LIFO
+	push 	msg		; then msg is pushed secondly to stack
+	call 	printf		; calls external function (printf) to print stack
+	mov 	eax, 0		; exit code for program
+	mov 	esp, ebp	; epilogue
+	pop 	ebp		; epilogue
+	ret			; epilogue
